20e3ae956e5ad985cc603cb5856abdc0a9e8b917
Amarjeet Kumar <141044796+Amarjeetkumar7@users.noreply.github.com>
2025-06-21 12:20:34 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -51,7 +51,8 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1DurfYaR74zl-MIXzt8RxJbwsmGn8gYbg/view?usp=sharing"
+            //href="https://drive.google.com/file/d/1DurfYaR74zl-MIXzt8RxJbwsmGn8gYbg/view?usp=sharing"
+            href="https://drive.google.com/file/d/1W-EudvG_Z4h_fn_ROpOLLYFP9GJBxZ0l/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
